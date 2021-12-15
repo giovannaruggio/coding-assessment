d9c721e356822df32c24a14f68a768d6438ef45d
Giovanna Ruggio <giovannaruggio@gmail.com>
2021-12-14 20:34:51 -0500

    Imrpove functions in js

--- assets/script.js
+++ assets/script.js
@@ -10,23 +10,22 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
-// Commonly used data types DO NOT include
-    // 1) strings, 2) booleans, 3) alerts, 4)numbers
-// The condition in an if/else statement is enclosed within ___.
-    // 1) quotes, 2) curly brackets, 3) parentheses, 4) square brackets
-// Arrays in JavaScript can be used to store ___.
-    // 1) numbers and strings, 2) other arrays, 3) booleans, 4) all of the above
-// String values must be enclosed within __ when being assigned to variables
-    // 1) commas, 2) curly brackets, 3) quotes, 4) parentheses
-// A very useful tool used during development and debugging fro printing content to the debugger is
-    // 1) Javascript, 2) terminal/bash, 3) for loops, 4) console.log
-
-
 var startButton = document.querySelector("#start");
 var questionContainer = document.querySelector(".question-container");
 var optionContainer = document.querySelector(".option-container");
 var headerContainer = document.querySelector("header");
+var highScores = document.querySelector("section");
+
+var question = document.querySelector("#question");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+var answer = document.querySelector("#scores");
+
 var position = 0;
+var score = 0;
+var seconds = 60;
 
 console.log(headerContainer);
 
@@ -39,28 +38,32 @@ startButton.addEventListener("click", function(){
 })
 
 function showQuestions() {
-    var question = document.querySelector("#question");
-    question.textContent = questions[position].question;
-    option1.textContent = questions[position].options[0];
-    var option2 = document.querySelector("#option2");
-    option2.textContent = questions[position].options[1];
-    var option3 = document.querySelector("#option3");
-    option3.textContent = questions[position].options[2];
-    var option4 = document.querySelector("#option4");
-    option4.textContent = questions[position].options[3];
-    option1.addEventListener("click", nextQuestion);
-    option2.addEventListener("click", nextQuestion);
-    option3.addEventListener("click", nextQuestion);
-    option4.addEventListener("click", nextQuestion);
+    question.textContent = quiz[position].question;
+    option1.textContent = quiz[position].options[0];
+    option2.textContent = quiz[position].options[1];
+    option3.textContent = quiz[position].options[2];
+    option4.textContent = quiz[position].options[3];
+    option1.addEventListener("click", checkAnswer);
+    option2.addEventListener("click", checkAnswer);
+    option3.addEventListener("click", checkAnswer);
+    option4.addEventListener("click", checkAnswer);
 }
 
-function nextQuestion() {
+function checkAnswer (event) {
+    console.log(event.target);
+    var answer = event.target.innerText;
+    if (answer === quiz[position].correct) {
+        alert ("Correct Answer!");
+    } else {
+        alert ("Wrong Answer!");
+    }
+    optionContainer.addEventListener("click", showQuestions);
     console.log("click");
-    position++
-    showQuestions()
+    position ++;
+    showQuestions();
 }
 
-var questions = [
+var quiz = [
     {
         question: "Commonly used data types DO NOT include:",
         options: ["strings", "booleans", "alerts", "numbers"],
@@ -85,5 +88,6 @@ var questions = [
         question: "What is a very useful tool used during development and debugging for printing content to the debugger?",
         options: ["Javascript", "terminal/bash", "for loops", "console.log"],
         correct: "Javascript"
-    }, 
+    } 
 ]
+
